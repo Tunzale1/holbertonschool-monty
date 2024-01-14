54ae7c25023eb03d1ded28a1ba0ebab94ac108e6
isagarazadeh <garazadeisa625@gmail.com>
2024-01-14 05:30:35 -0800

    push pall corrected version

--- pushpall.c
+++ pushpall.c
@@ -1,5 +1,4 @@
-#include "monty.h"
-
+u#include "monty.h"
 /**
  * pall - prints all the values on the stack, starting from
  * the top of the stack
@@ -8,6 +7,7 @@
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
+
 	(void)(line_number);
 
 	while (*stack)
@@ -22,28 +22,25 @@ void pall(stack_t **stack, unsigned int line_number)
  * @stack: Stack of nodes
  * @line_number: Current line number
  */
+
 void push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *aux = NULL;
 	int integer;
 
-	if (input[1] == NULL)
+	aux = malloc(sizeof(stack_t));
+	if (aux == NULL)
 	{
-		dprintf(2, "L%i: usage: push integer\n", line_number);
+		free(aux);
+		dprintf(2, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 
 	integer = atoi(input[1]);
-	if (integer == 0 && input[1][0] != '0')
+	if (integer == 0)
 	{
 		dprintf(2, "L%i: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	aux = malloc(sizeof(stack_t));
-	if (aux == NULL)
-	{
-		dprintf(2, "Error: malloc failed\n");
+		free(aux);
 		exit(EXIT_FAILURE);
 	}
 
